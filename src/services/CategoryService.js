723b54454e3009b348f35690638b17449d88b79e
Jonathan Irvin <djfoxyslpr@gmail.com>
2019-07-13 20:46:47 -0500

    feat(services): add category and gif service

--- src/services/CategoryService.js
+++ src/services/CategoryService.js
@@ -1,7 +1,50 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+const BASEURL = process.env.REACT_APP_API_BASEURL;
+export default class CategoryService {
+  static async createCategory(userId, categoryName, gifs) {
+    try {
+      return await axios.post(BASEURL + 'category', {
+        userId,
+        categoryName,
+        gifs,
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
 
-export default class CategoryService extends Component {
-  render() {
-    return <div />;
+  static async getCategory(id) {
+    try {
+      return await axios.get(BASEURL + 'category/' + id);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async getAllCategories(id) {
+    try {
+      return await axios.get(BASEURL + 'categories');
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async updateCategory(id, categoryName, gifs) {
+    try {
+      return await axios.put(BASEURL + 'category/' + id, {
+        categoryName,
+        gifs,
+      });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async deleteCategory(id) {
+    try {
+      return await axios.delete(BASEURL + 'category/' + id);
+    } catch (e) {
+      throw e;
+    }
   }
 }
--- src/services/GifService.js
+++ src/services/GifService.js
@@ -1,7 +1,43 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+const BASEURL = process.env.REACT_APP_API_BASEURL;
+export default class GifService {
+  static async addGifToProfile(userId, gifData) {
+    try {
+      return await axios.post(BASEURL + 'gif', { userId, gifData });
+    } catch (e) {
+      throw e;
+    }
+  }
 
-export default class GifService extends Component {
-  render() {
-    return <div />;
+  static async getGif(id) {
+    try {
+      return await axios.get(BASEURL + 'gif/' + id);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async getAllGifs(id) {
+    try {
+      return await axios.get(BASEURL + 'gifs');
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async updateGif(id, gifData) {
+    try {
+      return await axios.put(BASEURL + 'gif/' + id, { gifData });
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static async deleteGif(id) {
+    try {
+      return await axios.delete(BASEURL + 'gif/' + id);
+    } catch (e) {
+      throw e;
+    }
   }
 }
